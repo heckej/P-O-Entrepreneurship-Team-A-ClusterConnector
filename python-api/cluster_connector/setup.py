7108f9dbbcb3fe7d27db053a95b9758f970c2fa1
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-03-27 19:22:12 +0100

    Allowed Python versions added to setup

--- python-api/cluster_connector/setup.py
+++ python-api/cluster_connector/setup.py
@@ -10,6 +10,7 @@ setup(
     packages=['cluster'],
     # Needed for dependencies
     install_requires=['requests'],
+    python_requires='>=3.7, <4',
     # *strongly* suggested for sharing
     version='0.1.1',
     # The license can be anything you like
