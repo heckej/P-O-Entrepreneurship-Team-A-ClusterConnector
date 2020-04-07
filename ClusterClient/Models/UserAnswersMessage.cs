09fdb8b7542559670b2c407ff9107c27e150e5f7
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-07 10:19:14 +0200

    UserAnswersMessage with List property and helper functions

--- ClusterClient/Models/UserAnswersMessage.cs
+++ ClusterClient/Models/UserAnswersMessage.cs
@@ -1,16 +1,33 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+
 
 namespace ClusterClient.Models
 {
     public class UserAnswersMessage : UserMessage
     {
-        private readonly List<UserAnswer> AnswerQuestions = new List<UserAnswer>();
+        private List<UserAnswer> answerQuestions = new List<UserAnswer>();
 
-        public void addAnswer(UserAnswer answer)
+        public List<UserAnswer> AnswerQuestions {
+            get  => this.answerQuestions;
+            set
+            {
+                foreach(UserAnswer answer in value)
+                    this.AddAnswer(answer);
+            }
+        }
+
+        public void AddAnswer(UserAnswer answer)
         {
-            this.AnswerQuestions.Add(answer);
+            this.answerQuestions.Add(answer);
+        }
+
+        public void AddAnswers(ICollection<UserAnswer> userAnswers)
+        {
+            this.answerQuestions = this.answerQuestions.Union(userAnswers).ToList();
+            //((ISet<UserAnswer>) this.answerQuestions).UnionWith(userAnswers);
         }
     }
 }
