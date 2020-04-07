c09c6e34b38438f9aa97d451531073f3f20d35f5
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-07 10:35:09 +0200

    Imports fixed

--- ClusterClient/Connector.cs
+++ ClusterClient/Connector.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using ClusterClient.Models;
