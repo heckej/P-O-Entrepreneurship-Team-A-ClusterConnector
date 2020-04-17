d870992c06885f0f66bbffd4de07948334dcbbbe
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-17 22:28:27 +0200

    Requests constant values

---
+++ ClusterClient/Models/Requests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ClusterClient.Models
+{
+    public class Requests
+    {
+        public const string UnansweredQuestions = "answer_questions";
+        public const string Default = "no_request";
+        public static ISet<string> GetRequests()
+        {
+            return new HashSet<string>
+            {
+                UnansweredQuestions,
+            };
+        }
+    }
+}
