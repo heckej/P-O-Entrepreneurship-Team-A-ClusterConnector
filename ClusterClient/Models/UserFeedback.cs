249b14e06e57ae7bb292b619edbb8b0ebf0c226e
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-20 16:21:07 +0200

    Models toJson() method added

--- ClusterClient/Models/UserFeedback.cs
+++ ClusterClient/Models/UserFeedback.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace ClusterClient.Models
 {
@@ -9,5 +10,9 @@ namespace ClusterClient.Models
         public int question_id { get; set; }
         public int answer_id { get; set; }
         public int feedback_code { get; set; }
+        public override string ToJson()
+        {
+            return JsonSerializer.Serialize<UserFeedback>(this);
+        }
     }
 }
