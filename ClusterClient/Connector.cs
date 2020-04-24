fbceaa2591c55f33a364ebe468fd0ad18103bfc9
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-24 19:25:09 +0200

    SendQuestionAsync deprecated

--- ClusterClient/Connector.cs
+++ ClusterClient/Connector.cs
@@ -387,10 +387,23 @@ namespace ClusterClient
         /// In case the <c>answer</c> property of the returned server answer is <c>null</c>, then the server the server has assigned a 
         /// question ID to the given question, but it hasn't found an answer yet.</returns>
         /// <exception cref="Exception">The websocket thread has passed an exception. The passed exception is thrown by this method.</exception>
-        [Obsolete("SendQuestion is deprecated, please use SendQuestionAsync instead.")]
-        public async Task<ServerAnswer> SendQuestion(string userID, string question, double timeout=5)
+        [Obsolete("SendQuestionAsync is deprecated, please use SendQuestionAndWaitForAnswer instead.")]
+        public async Task<ServerAnswer> SendQuestionAsync(string userID, string question, double timeout = 5)
         {
-            return await this.SendQuestionAsync(userID, question, timeout);
+            Console.WriteLine("Send question method called.");
+            this.CheckoutWebSocket();
+            UserQuestion request = new UserQuestion
+            {
+                user_id = userID,
+                question = question,
+                chatbot_temp_id = this.GetNextTempChatbotID()
+            };
+            this.AddMessageToSendQueue(request);
+            var answer = await Task.Run(() => this.GetAnswerFromServerToQuestion(request.chatbot_temp_id, userID, timeout));
+            /*if (answer == null)
+                throw new TimeoutException("No response was received from the server to this question, so no question ID could be assigned. " +
+                    "Try again later or use a higher timeout.");*/
+            return answer;
         }
 
         /// <summary>
