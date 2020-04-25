80aa5d37f03c85e73352785d8edc1b20efb75080
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-25 23:56:53 +0200

    Default answer_id of server answer: -1

--- ClusterClient/Models/ServerAnswer.cs
+++ ClusterClient/Models/ServerAnswer.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace ClusterClient.Models
 {
+    private int _answer_id = -1;
     public class ServerAnswer : ServerMessage
     {
         public int question_id { get; set; }
         public string question { get; set; }
-        public int answer_id { get; set; }
+        public int answer_id { get _answer_id; set => _answer_id = value; }
         public string answer { get; set; }
         public int chatbot_temp_id { get; set; }
         public double certainty { get; set; }
