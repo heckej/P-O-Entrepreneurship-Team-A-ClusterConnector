ac8e710cca45f08f87d9a8b0c195bd7a59587d93
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-27 18:10:35 +0200

    Revert "SendMessagesToEndPointAsync() made private + catch exceptions"
    This reverts commit a19ac013d5669edbc72dc722eba46a750c5060ba.

--- ClusterClient/Connector.cs
+++ ClusterClient/Connector.cs
@@ -257,7 +257,7 @@ namespace ClusterClient
         /// </summary>
         /// <param name="content">The json string to be sent.</param>
         /// <returns>True if and only if the end point address of this connector is set and the response code is 2xx.</returns>
-        private async Task<bool> SendMessageToEndPointAsync(string content, string route = Actions.Default)
+        public async Task<bool> SendMessageToEndPointAsync(string content, string route = Actions.Default)
         {
             Console.WriteLine("Sending proactive message: " + content);
             var httpContent = new StringContent(content, Encoding.UTF8, "application/json");
@@ -267,19 +267,9 @@ namespace ClusterClient
                 Method = HttpMethod.Post
             };
             httpRequest.Headers.Add("Authorization", this.authorization);
-            try
-            {
-                var response = await Connector.httpClient.PostAsync(this.EndPointAddress + "/" + route, httpContent);
-                Console.WriteLine("Response status code: " + response.StatusCode);
-                response.EnsureSuccessStatusCode();
-                return true;
-            } 
-            catch(Exception)
-            {
-                // Proactive message blocked by end point.
-                return false;
-            }
-            
+            var response = await Connector.httpClient.PostAsync(this.EndPointAddress + "/" + route, httpContent);
+            Console.WriteLine("Response status code: " + response.StatusCode);
+            return (int)response.StatusCode >= 200 && (int)response.StatusCode < 300;
 
         }
 
