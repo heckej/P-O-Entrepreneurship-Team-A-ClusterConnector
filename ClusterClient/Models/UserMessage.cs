c03552e24991325e50c4f0fb388de0163d8772fe
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-27 20:57:23 +0200

    UserMessage with action + feedback action

--- ClusterClient/Models/UserMessage.cs
+++ ClusterClient/Models/UserMessage.cs
@@ -7,8 +7,10 @@ namespace ClusterClient.Models
     public abstract class UserMessage
     {
         private string _user_id = null;
+        private string _action = Actions.Default;
 
         public string user_id { get => _user_id; set => _user_id = value; }
+        public string action { get => _action; set => _action = value; }
 
         public abstract string ToJson();
     }
